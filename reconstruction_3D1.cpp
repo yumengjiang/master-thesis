34f83fe7985b5d8ecd5298aebfd4f52d355013cb
liweim <liweim@github.com>
2018-03-21 22:52:01 +0100

    convert to opencv style

--- reconstruction_3D1.cpp
+++ reconstruction_3D1.cpp
@@ -9,116 +9,75 @@
 #include "limits.h"
 #include "eigen3/Eigen/Core"
 #include "eigen3/Eigen/Dense"
+#include <opencv2/features2d.hpp>
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/opencv.hpp>
+#include "opencv2/xfeatures2d.hpp"
+#include <opencv2/highgui.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
 
 using namespace std;
 
-double threshold = 50;
+float threshold = 50;
 
-double computeResiduals(int beforeX, int afterX, int beforeY, int afterY){
-  return pow((pow ((afterX-beforeX),2) + pow ((afterY-beforeY),2)),0.5);
+float computeResiduals(cv::Point2f pt1, cv::Point2f pt2){
+  return pow((pow ((pt1.x-pt2.x),2) + pow ((pt1.y-pt2.y),2)),0.5);
 }
 
-Eigen::MatrixXi matchFeatures(Eigen::MatrixXi matchedLast, 
-                  Eigen::MatrixXi featureLast, Eigen::MatrixXi featureNext){
-  double res, minRes;
-  int featureLastRow = featureLast.rows();
-  int featureNextRow = featureNext.rows();
-  int matchedLastRow = matchedLast.rows();
-  int matchedLastCol = matchedLast.cols();
-  int count = matchedLastRow;
-  int index, indexCandidate;
-  Eigen::MatrixXi matchedNextTmp(matchedLastRow+featureNextRow, matchedLastCol+1);
-  for(int i = 0; i < matchedLastRow; i++)
-    for(int j = 0; j < matchedLastCol; j++)
-      matchedNextTmp(i, j) = matchedLast(i, j);
+void matchFeatures(int imageId, vector<cv::KeyPoint> featureLast, 
+                  vector<cv::KeyPoint> featureNext, vector<cv::DMatch> &matched){
+  float res, minRes;
+  int featureLastRow = featureLast.size();
+  int featureNextRow = featureNext.size();
+  int index;
 
   for(int i = 0; i < featureNextRow; i++){
     minRes = threshold;
     for(int j = 0; j < featureLastRow; j++){
-      if(featureNext(i,2) == featureLast(j,2)){
-        res = computeResiduals(featureNext(i,0), featureLast(j,0), featureNext(i,1), featureLast(j,1));//check residuals, find the smallest one, save it
+      if(featureNext[i].class_id == featureLast[j].class_id){
+        res = computeResiduals(featureNext[i].pt, featureLast[j].pt);//check residuals, find the smallest one, save it
         if(res < minRes){
           minRes = res;
           index = j;
         }
       }
     }
-    if(minRes == threshold){
-      matchedNextTmp(count++, matchedLastCol) = i+1;
+    if(minRes < threshold){
+      matched.push_back(cv::DMatch(index,i,imageId,minRes));
+      cout << index << " " << i << " " << imageId << " " << minRes << endl;
     } 
-    else{
-      indexCandidate = matchedNextTmp(index, matchedLastCol)-1;
-      if(indexCandidate+1 == 0){
-        matchedNextTmp(index, matchedLastCol) = i+1;
-      }
-      else{
-        res = computeResiduals(featureNext(i,0), featureLast(indexCandidate,0), featureNext(i,1), featureLast(indexCandidate,1));//check residuals, find the smallest one, save it
-        if(res < minRes){
-          matchedNextTmp(count++, matchedLastCol) = i+1;
-        }
-        else{
-          matchedNextTmp(index, matchedLastCol) = i+1;
-          matchedNextTmp(count++, matchedLastCol) = indexCandidate+1;
-        }
-      }
-    }
   }
-  Eigen::MatrixXi matchedNext(count, matchedLastCol+1);
-  for(int i = 0; i < count; i++)
-    for(int j = 0; j < matchedLastCol+1; j++)
-      matchedNext(i, j) = matchedNextTmp(i, j);
-  return matchedNext;
 }
-
 
 int main( int argc, char** argv )
 {
-  Eigen::MatrixXi featureLast(10, 3);
-  Eigen::MatrixXi featureNext(10, 3);
+  vector<cv::KeyPoint> featureLast, featureNext;
 
-  featureLast << 299,198,1,
-                  688,199,1,
-                  143,201,1,
-                  455,187,1,
-                  455,195,2,
-                  276,180,2,
-                  161,183,2,
-                  612,208,2,
-                  234,211,3,
-                  510,223,3;
+  featureLast.push_back(cv::KeyPoint(299,198,3,-1,0,0,1));
+  featureLast.push_back(cv::KeyPoint(688,199,3,-1,0,0,1));
+  featureLast.push_back(cv::KeyPoint(143,201,3,-1,0,0,1));
+  featureLast.push_back(cv::KeyPoint(455,187,3,-1,0,0,1));
+  featureLast.push_back(cv::KeyPoint(455,195,3,-1,0,0,2));
+  featureLast.push_back(cv::KeyPoint(276,180,3,-1,0,0,2));
+  featureLast.push_back(cv::KeyPoint(161,183,3,-1,0,0,2));
+  featureLast.push_back(cv::KeyPoint(612,208,3,-1,0,0,2));
+  featureLast.push_back(cv::KeyPoint(234,211,3,-1,0,0,3));
+  featureLast.push_back(cv::KeyPoint(510,223,3,-1,0,0,3));
 
-  featureNext << 280,209,1,
-                  624,207,1,
-                  457,194,1,
-                  113,214,1,
-                  268,188,2,
-                  462,204,2,
-                  142,192,2,
-                  324,181,2,
-                  538,241,3,
-                  209,228,3;
+  featureNext.push_back(cv::KeyPoint(280,209,3,-1,0,0,1));
+  featureNext.push_back(cv::KeyPoint(624,207,3,-1,0,0,1));
+  featureNext.push_back(cv::KeyPoint(457,194,3,-1,0,0,1));
+  featureNext.push_back(cv::KeyPoint(113,214,3,-1,0,0,1));
+  featureNext.push_back(cv::KeyPoint(268,188,3,-1,0,0,2));
+  featureNext.push_back(cv::KeyPoint(462,204,3,-1,0,0,2));
+  featureNext.push_back(cv::KeyPoint(142,192,3,-1,0,0,2));
+  featureNext.push_back(cv::KeyPoint(324,181,3,-1,0,0,2));
+  featureNext.push_back(cv::KeyPoint(538,241,3,-1,0,0,3));
+  featureNext.push_back(cv::KeyPoint(209,228,3,-1,0,0,3));
 
-  // featureNext << 299,198,1,
-  //                 688,199,1,
-  //                 143,201,1,
-  //                 455,187,1,
-  //                 455,195,2,
-  //                 276,180,2,
-  //                 161,183,2,
-  //                 612,208,2,
-  //                 234,211,3,
-  //                 1,1,1,
-  //                 1,1,1,
-  //                 510,223,3;
-
-  int featureLastRow = featureLast.rows();
-  Eigen::MatrixXi matchedNext, matchedLast(featureLastRow, 1);
-  for(int i = 0; i < featureLastRow; i++){
-    matchedLast(i, 0) = i+1;
-  }
-
-  matchedNext = matchFeatures(matchedLast, featureLast, featureNext);
-  std::cout << matchedNext << std::endl;
-  return 0;
+  vector<cv::DMatch> matched;
+  matchFeatures(1, featureLast, featureNext, matched);
 
 }
